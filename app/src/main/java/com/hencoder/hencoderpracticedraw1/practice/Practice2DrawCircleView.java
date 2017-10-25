25cd4cc76279c62cf5376acb99233b3bdee4603c
Dylan_Wang <dylan_wang@asus.com>
2017-10-25 11:26:16 +0800

    finish Practice2DrawCircleView

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,6 +25,27 @@ public class Practice2DrawCircleView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(200, 150, 120, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2);
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(500, 150, 120, paint);
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setColor(Color.rgb(100, 100, 200));
+        canvas.drawCircle(200, 420, 120, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(500, 420, 120, paint);
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
