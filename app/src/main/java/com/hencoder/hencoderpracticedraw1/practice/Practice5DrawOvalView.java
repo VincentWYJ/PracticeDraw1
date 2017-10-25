1ad9f4605c63d8970ffb4caecc47fb5524a6ac3e
Dylan_Wang <dylan_wang@asus.com>
2017-10-25 14:21:31 +0800

    finish Practice4D5DrawPointVoew

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
@@ -2,11 +2,13 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice5DrawOvalView extends View {
+    Paint mPaint = new Paint();
 
     public Practice5DrawOvalView(Context context) {
         super(context);
@@ -23,6 +25,11 @@ public class Practice5DrawOvalView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        mPaint.setAntiAlias(true);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawOval(250, 200, 500, 350, mPaint);
 
 //        练习内容：使用 canvas.drawOval() 方法画椭圆
     }
