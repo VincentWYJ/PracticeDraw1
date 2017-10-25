10758fb3f46feba81f0468b6043cc842419ef974
Dylan_Wang <dylan_wang@asus.com>
2017-10-25 14:35:46 +0800

    finish Practice6DrawLineView

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
@@ -2,11 +2,14 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice6DrawLineView extends View {
+    Paint mPaint = new Paint();
 
     public Practice6DrawLineView(Context context) {
         super(context);
@@ -23,6 +26,12 @@ public class Practice6DrawLineView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        mPaint.setAntiAlias(true);
+
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(8);
+        canvas.drawLine(200, 200, 450, 400, mPaint);
 
 //        练习内容：使用 canvas.drawLine() 方法画直线
     }
